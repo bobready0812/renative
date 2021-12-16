5fd5ae5561b92daae76ec4df39952864bd894968
DaehyeonKim <Daehyeonoff@gmail.com>
2021-12-16 22:34:27 +0900

    20211216

--- App.js
+++ App.js
@@ -7,65 +7,73 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
+import type {Node}
+from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors, DebugInstructions, Header, LearnMoreLinks, ReloadInstructions} from 'react-native/Libraries/NewAppScreen';
+import { Fragment } from 'react/cjs/react.production.min';
 
 const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+    const isDarkMode = useColorScheme() === 'dark';
+    return (
+        <View style={styles.sectionContainer}>
+            <Text
+                style={[
+                    styles.sectionTitle, {
+                        color: isDarkMode
+                            ? Colors.white
+                            : Colors.black
+                    }
+                ]}>
+                {title}
+            </Text>
+            <Text
+                style={[
+                    styles.sectionDescription, {
+                        color: isDarkMode
+                            ? Colors.light
+                            : Colors.dark
+                    }
+                ]}>
+                {children}
+            </Text>
+        </View>
+    );
 };
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+    const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    const backgroundStyle = {
+        backgroundColor: isDarkMode
+            ? Colors.darker
+            : Colors.lighter
+    };
 
-  return (
-    <Text>안녕</Text>
-  );
+    return (
+      <View>
+        <View>
+            <Text>안녕</Text>
+        </View>
+        <View>
+            <Text>안녕</Text>
+        </View>
+        <View>
+            <Text>안녕</Text>
+        </View>
+      </View>
+    );
 };
 
-const styles = StyleSheet.create({
- 
-});
+const styles = StyleSheet.create({});
 
 export default App;
