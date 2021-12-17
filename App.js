1c4d26176507edca1a198c8c346fca630dcda855
DaehyeonKim <Daehyeonoff@gmail.com>
2021-12-17 21:57:25 +0900

    20211217

--- App.js
+++ App.js
@@ -21,54 +21,16 @@ import {
 } from 'react-native';
 
 import {Colors, DebugInstructions, Header, LearnMoreLinks, ReloadInstructions} from 'react-native/Libraries/NewAppScreen';
-import {Fragment} from 'react/cjs/react.production.min';
+import {Fragment, useState} from 'react/cjs/react.production.min';
 
-const Section = ({children, title}): Node => {
-    const isDarkMode = useColorScheme() === 'dark';
-    return (
-        <View style={styles.sectionContainer}>
-            <Text
-                style={[
-                    styles.sectionTitle, {
-                        color: isDarkMode
-                            ? Colors.white
-                            : Colors.black
-                    }
-                ]}>
-                {title}
-            </Text>
-            <Text
-                style={[
-                    styles.sectionDescription, {
-                        color: isDarkMode
-                            ? Colors.light
-                            : Colors.dark
-                    }
-                ]}>
-                {children}
-            </Text>
-        </View>
-    );
-};
 
 const App: () => Node = () => {
-    const isDarkMode = useColorScheme() === 'dark';
 
-    const backgroundStyle = {
-        backgroundColor: isDarkMode
-            ? Colors.darker
-            : Colors.lighter
-    };
-
-    return (
-        <View>
-            <View>
-                <TextInput  style={styles.input1}>
-
-                </TextInput>
-            </View>
-        </View>
-    );
+  return( 
+      <Text>안녕</Text>
+  )
+    
+    ;
 };
 
 const styles = StyleSheet.create({
